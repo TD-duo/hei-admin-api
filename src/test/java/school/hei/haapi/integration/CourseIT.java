7ec3b247195247cbe57ba502361132e5fe728e82
Ranomenjanahary <hei.jaonary@gmail.com>
2023-03-24 14:50:50 +0300

    feat: add test on courseIT

--- src/test/java/school/hei/haapi/integration/CourseIT.java
+++ src/test/java/school/hei/haapi/integration/CourseIT.java
@@ -18,6 +18,7 @@ import school.hei.haapi.endpoint.rest.model.UpdateStudentCourse;
 import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
 import school.hei.haapi.integration.conf.TestUtils;
+import school.hei.haapi.model.Course;
 
 import java.util.List;
 
@@ -120,7 +121,22 @@ public class CourseIT {
     toCreate.setMainTeacherId(null);
     return toCreate;
   }
+  @Test
+  void fees_pages_are_ordered_by_due_datetime_desc() throws ApiException {
+    ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
+    PayingApi api = new PayingApi(student1Client);
+    int pageSize = 2;
 
+    List<Fee> page1 = api.getStudentFees(STUDENT1_ID, 1, pageSize, null);
+    List<Fee> page2 = api.getStudentFees(STUDENT1_ID, 2, pageSize, null);
+    List<Fee> page3 = api.getStudentFees(STUDENT1_ID, 3, pageSize, null);
+
+    assertEquals(pageSize, page1.size());
+    assertEquals(2, page2.size());
+    assertEquals(0, page3.size());
+    assertTrue(isAfter(page1.get(0).getDueDatetime(), page1.get(1).getDueDatetime()));
+    assertTrue(isAfter(page1.get(1).getDueDatetime(), page2.get(0).getDueDatetime()));
+  }
   public static Course CreatedCourse1() {
     Course createdCourse1 = new Course();
     createdCourse1.setName(toCreateSuccess().getName());
@@ -147,16 +163,73 @@ public class CourseIT {
   }
 
   @Test
-  void manager_read_ok() throws ApiException {
-    ApiClient apiClient = anApiClient(MANAGER1_TOKEN);
+  void student_read_sorted_by_code_asc() throws ApiException {
+    ApiClient apiClient = anApiClient(STUDENT1_TOKEN);
     TeachingApi api = new TeachingApi(apiClient);
 
-    List<Course> actual = api.getCourses(1, 15);
+    List<Course> actual = api.getCourses(1, 15, null, null, null,
+            null, null, null, Order.ASC);
 
-    assertEquals(5, actual.size());
-    assertTrue(actual.containsAll(List.of(course1(), course2())));
+    assertEquals(3, actual.size());
+    assertEquals(course1(), actual.get(0));
+    assertEquals(course2(), actual.get(1));
+    assertEquals(course3(), actual.get(2));
   }
 
+  @Test
+  void student_read_ko_invalid_credentials() {
+    ApiClient apiClient = anApiClient(BAD_TOKEN);
+    TeachingApi api = new TeachingApi(apiClient);
+
+    assertThrowsApiException(
+            "{\"type\":\"401 UNAUTHORIZED\",\"message\":\"Full authentication is required to access this resource\"}",
+            () -> api.getCourses(1, 15, null, null, null,
+                    null, null, null, null));
+  }
+
+  @Test
+  void student_read_ko_invalid_page() {
+    ApiClient apiClient = anApiClient(STUDENT1_TOKEN);
+    TeachingApi api = new TeachingApi(apiClient);
+
+    assertThrowsApiException(
+            "{\"type\":\"400 BAD_REQUEST\",\"message\":\"page value must be >=1\"}",
+            () -> api.getCourses(-1, 15, null, null, null,
+                    null, null, null, null));
+  }
+
+  @Test
+  void student_read_ko_invalid_page_size() {
+    ApiClient apiClient = anApiClient(STUDENT1_TOKEN);
+    TeachingApi api = new TeachingApi(apiClient);
+
+    assertThrowsApiException(
+            "{\"type\":\"400 BAD_REQUEST\",\"message\":\"page size value must be between 5 and 20\"}",
+            () -> api.getCourses(1, 4, null, null, null,
+                    null, null, null, null));
+  }
+
+  @Test
+  void student_read_ko_no_results() {
+    ApiClient apiClient = anApiClient(STUDENT1_TOKEN);
+    TeachingApi api = new TeachingApi(apiClient);
+
+    assertThrowsApiException(
+            "{\"type\":\"404 NOT_FOUND\",\"message\":\"No courses found\"}",
+            () -> api.getCourses(2, 15, null, null, null,
+                    null, null, null, null));
+  }
+
+  @Test
+  void student_read_ko_invalid_teacher_name() {
+    ApiClient apiClient = anApiClient(STUDENT1_TOKEN);
+    TeachingApi api = new TeachingApi(apiClient);
+
+    assertThrowsApiException(
+            "{\"type\":\"400 BAD_REQUEST\",\"message\":\"Invalid teacher name\"}",
+            () -> api.getCourses(1, 15, null, null, null,
+                    "123", "Smith", null, null));
+  }
   static class ContextInitializer extends AbstractContextInitializer {
     public static final int SERVER_PORT = anAvailableRandomPort();
 
